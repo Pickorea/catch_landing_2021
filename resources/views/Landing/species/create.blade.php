d7683de230f7872cf0d60e5dedcb2a900c66b71c
Kairaoi Ientumoa <pickorea788@gmail.com>
2021-02-26 16:05:19 +1200

    updates forms

--- resources/views/Landing/species/create.blade.php
+++ resources/views/Landing/species/create.blade.php
@@ -5,22 +5,18 @@
     <div class="row justify-content-center">
         <div class="col-md-12">
             <div class="card">
-                <div class="card-header">{{ __('Create Species') }}</div>
-               
-
-                <div class="card-body">
-                   
-                {{ html()->form('POST', route('species.store'))->open() }}
-                <div class="form-group">
-                    <label for="species_name">Species Name</label>
-                    <input type="text" class="form-control" id="species_name" name="species_name" placeholder="Enter Species Name" required maxlength="50"  value="{{old('species_name')}}">
-                </div>
-                    
-                <button type="submit" class="btn btn-primary mb-2">Submit</button>
-                {{ html()->form()->close() }}
-
-
-                </div>
+                <x-forms.post action="{{ route('species.store') }}">
+                    <div class="card">
+                        <div class="card-header">{{ __('Create Species') }}
+                        </div>
+                        <div class="card-body">
+                            <x-forms.textfield type="text" name="species_name" label="Species Name" required/>
+                        </div>
+                        <div class="card-footer">
+                            <button type="submit" class="btn btn-primary mb-2">Submit</button>
+                        </div>
+                    </div>
+                </x-forms.post>          
             </div>
         </div>
     </div>
--- resources/views/Landing/species/edit.blade.php
+++ resources/views/Landing/species/edit.blade.php
@@ -5,22 +5,19 @@
     <div class="row justify-content-center">
         <div class="col-md-12">
             <div class="card">
-                <div class="card-header">{{ __('Edit Species') }}</div>
-               
-
-                <div class="card-body">
-                   
-                {{ html()->form('PATCH', route('species.update', $item->id))->open() }}
-                <div class="form-group">
-                    <label for="species_name">Species Name</label>
-                    <input type="text" class="form-control" id="species_name" name="species_name" placeholder="Enter species Name" required maxlength="50"  value="{{old('species_name', $item->species_name)}}">
-                </div>
-                    
-                <button type="submit" class="btn btn-primary mb-2">Update</button>
-                {{ html()->form()->close() }}
-
-
-                </div>
+                <x-forms.patch action="{{ route('species.update', $item->id) }}">
+                    <div class="card">
+                        <div class="card-header">{{ __('Create Species') }}
+                        </div>
+                        <div class="card-body">
+                            <x-forms.textfield type="text" name="species_name" label="Species Name" value="{{$item->species_name}}"/>
+                        </div>
+                        <div class="card-footer">
+                            <button type="submit" class="btn btn-primary mb-2">Submit</button>
+                        </div>
+                    </div>
+                </x-forms.patch>   
+                
             </div>
         </div>
     </div>
