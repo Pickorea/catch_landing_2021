ad750fbbfac356e731281263411f392dc7a5f4ab
Kairaoi Ientumoa <pickorea788@gmail.com>
2021-03-04 13:51:36 +1200

    speciesservices

--- app/Http/Controllers/SpeciesController.php
+++ app/Http/Controllers/SpeciesController.php
@@ -7,9 +7,24 @@ use Illuminate\Support\Facades\Paginator;
 use App\Http\Requests\Landing\StoreSpeciesRequest;
 use App\Http\Requests\Landing\UpdateSpeciesRequest;
 use App\Models\Species;
+use App\Services\SpeciesService;
 
 class SpeciesController extends Controller
 {
+      /**
+     * @var SpeciesService
+     */
+    protected $service;
+
+    /**
+    * SpeciesController constructor.
+    * @param SpeciesService $service
+    */
+    public function __construct(SpeciesService $service)
+    {
+        $this->service = $service;
+    }
+
     /**
      * Display a listing of the resource.
      *
@@ -39,7 +54,7 @@ class SpeciesController extends Controller
      */
     public function store(StoreSpeciesRequest $request)
     {
-        $species = Species::create($request->all());
+        $item = $this->service->store($request->validated());
 
         return redirect()->route('species.index')
         ->with('success', 'Species created successfully');
@@ -74,9 +89,9 @@ class SpeciesController extends Controller
      * @param  int  $id
      * @return \Illuminate\Http\Response
      */
-    public function update(Request $request, Species $species)
+    public function update(UpdateSpeciesRequest $request, Species $species)
     {
-        $species->update($request->all());
+        $this->service->update($species, $request->validated());
     
         return redirect()->route('species.index')
                         ->with('success', 'species updated successfully');
@@ -88,8 +103,11 @@ class SpeciesController extends Controller
      * @param  int  $id
      * @return \Illuminate\Http\Response
      */
-    public function destroy($id)
+    public function destroy(Species $species)
     {
-        //
+        $this->service->delete($species);
+
+        return redirect()->route('species.index')
+        ->with('success', 'Product deleted successfully');
     }
 }
---
+++ app/Services/SpeciesService.php
@@ -0,0 +1,99 @@
+<?php
+
+namespace App\Services;
+
+use App\Models\Species;
+use App\Exceptions\GeneralException;
+use App\Services\BaseService;
+use Illuminate\Support\Facades\Auth;
+
+
+/**
+ * Class SpeciesService.
+ */
+class SpeciesService extends BaseService
+{
+    /**
+     * SpeciesService constructor.
+     *
+     * @param  Species  $species
+     */
+    public function __construct(Species $species)
+    {
+        $this->model = $species;
+    }
+
+    public function searchPaginate($search = null)
+    {
+        $user = Auth::user();
+        if (! $user->can('kiims.view')) {
+            throw new GeneralException(__('You do not have access to do that.'));
+        }
+
+        $query = $this->model->query();
+        if (! empty($search)) {
+            $search = '%'.$search.'%';
+            $query->where('name', 'like', $search)
+                ->orWhere('scientific_name', 'like', $search);
+        }
+
+        return $query->paginate();
+    }
+
+    public function datatables($search = '')
+    {
+        $user = Auth::user();
+        if (! $user->can('kiims.view')) {
+            throw new GeneralException(__('You do not have access to do that.'));
+        }
+        $query = $this->model->query();
+
+        if (! empty($search)) {
+            $query->whereLike(['name','scientific_name'], $search);
+        }
+
+        return $query;
+    }
+
+
+    /**
+     * @param array $data
+     * @return Species
+     * @throws GeneralException
+     */
+    public function store(array $data = []): Species
+    {
+        // $user = Auth::user();
+        // if (! $user->can('kiims.create')) {
+        //     throw new GeneralException(__('Not Authorised'));
+        // }
+
+        return  $this->model::create($data);
+    }
+
+
+    /**
+     * @param Species $item
+     * @param array $data
+     * @return Crime
+     */
+    public function update(Species $item, array $data = []): bool
+    {
+        // $user = Auth::user();
+        // if (! $user->can('kiims.edit')) {
+        //     throw new GeneralException(__('You do not have access to do that.'));
+        // }
+
+        return $item->update($data);
+    }
+
+    public function delete(Species $item, array $data = []): bool
+    {
+        // $user = Auth::user();
+        // if (! $user->can('kiims.edit')) {
+        //     throw new GeneralException(__('You do not have access to do that.'));
+        // }
+
+        return $item->delete($data);
+    }
+}
