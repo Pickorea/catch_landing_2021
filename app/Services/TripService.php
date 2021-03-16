a5871de725ae330d2d6e68f9b4f1a875528e94f3
Bryan <bryan.scott@ffa.int>
2021-03-16 14:11:28 +1100

    Update TripService.php

--- app/Services/TripService.php
+++ app/Services/TripService.php
@@ -61,13 +61,13 @@ class TripService extends BaseService
             ->leftjoin('locations', 'locations.id', '=', 'trips.location_id')
             ->leftjoin('islands', 'islands.id', '=', 'fishermans.island_id')
             ->select([
-                'trips.id',  'fisherman_id',  'island_name', 'first_name',  'last_name',
+                'trips.id',  'fisherman_id', 'island_name', 'first_name',  'last_name',
                 'trip_hrs', 'number_of_fishers', 'trip_date', 'location_name', 'method_name',
                 'trips.created_at'
             ]);
 
         if (! empty($search)) {
-            $query->where('first_name','last_name','LIKE',$search);
+            $query->whereLike(['first_name','last_name', 'island_name', 'location_name', 'method_name'],$search);
         }
 
         return $query;
